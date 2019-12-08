f6845aa8dfe785d6a025e698ac2e227638cb0fa0
tianyuecao <854069858@qq.com>
2019-12-08 00:07:44 -0800

    create solve_angle class

---
+++ energy/src/energy/solve_angle/solve_angle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vanessa on 12/7/19.
+//
+
+#include "solve_angle.h"
---
+++ energy/src/energy/solve_angle/solve_angle.h
@@ -0,0 +1,14 @@
+//
+// Created by vanessa on 12/7/19.
+//
+
+#ifndef SJTU_RM_CV_SOLVE_ANGLE_H
+#define SJTU_RM_CV_SOLVE_ANGLE_H
+
+
+class solve_angle {
+
+};
+
+
+#endif //SJTU_RM_CV_SOLVE_ANGLE_H
